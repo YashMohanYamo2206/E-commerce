1e9fd2c4cc3626be0c0bf71c0b8220f74c5e10ce
Yash Mohan <yashmoha2206@gmail.com>
2022-02-06 14:55:54 +0530

    added search field

---
+++ src/components/header/headerLogin.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const HeaderLogin = () => {
+    return <input className="header__right--login" type="submit"
+        value="Login" />
+}
+
+export default HeaderLogin;
---
+++ src/components/search/search.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { IoIosSearch } from 'react-icons/io';
+
+const Search = () => {
+
+    const submitForm = (e) => {
+        e.preventDefault();
+        console.log("submitted");
+    }
+    return (
+        <form className="search__form" onSubmit={(e) => submitForm(e)}>
+            <IoIosSearch className="search__icon" />
+            <input
+                className="search"
+                type="text"
+                placeholder="search"
+            />
+        </form>
+    );
+}
+
+export default Search;
