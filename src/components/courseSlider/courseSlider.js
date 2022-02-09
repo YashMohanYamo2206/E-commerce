13550cf61e5ed9cc4304d3c52910ee160c5933bd
Yash Mohan <yashmoha2206@gmail.com>
2022-02-09 23:13:22 +0530

    finished course slider

--- src/components/courseSlider/courseSlider.js
+++ src/components/courseSlider/courseSlider.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { courses } from "../../dummyData/courses";
 import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
+import useWindowDimensions from './windowResizeListener';
+// import Variables from '../../scss/_variables.scss';
 
 let courseCopy = [...courses];
 let courseList = courseCopy.map((course) => {
@@ -24,42 +26,70 @@ let courseList = courseCopy.map((course) => {
 
 
 const CourseSlider = () => {
+    const { height, width } = useWindowDimensions();
     const [courseCount, setCourseCount] = useState(0);
-    const [courseSliderAnimate, setcourseSliderAnimate] = useState('course-slider__courses');
-    const changeCourseCount = (e, forward) => {
-        e.stopPropagation();
-        forward ?
-            setcourseSliderAnimate('course-slider__courses course-slider__slideout') :
-            setcourseSliderAnimate('course-slider__courses course-slider__slideout-right');
-        setTimeout(() => {
-            if (forward) {
-                setCourseCount(Math.min(courseCount + showCourses, courses.length-1))
-                setcourseSliderAnimate('course-slider__courses course-slider__slidein')
-            }
-            else{
-                setCourseCount(Math.max(courseCount - showCourses, 0))
-                setcourseSliderAnimate('course-slider__courses course-slider__slidein-right')
-            }
-        }, 150)
-    };
+    const [courseSliderAnimate, setCourseSliderAnimate] = useState('course-slider__courses');
+
+
+
     let showCourses = 3;
     let activeList = [];
-    showCourses === 3 ? activeList = [courseList[courseCount], courseList[courseCount + 1], courseList[courseCount + 2]] :
-        activeList = [courseList[courseCount], courseList[courseCount + 1]];
+    console.log(width)
+    if (width <= 596) {
+        showCourses = 1;
+    } else if (width <= 768) {
+        showCourses = 2;
+    } else {
+        showCourses = 3;
+    }
+
+    for (let i = 0; i < showCourses; ++i) {
+        activeList.push(courseList[courseCount + i]);
+    }
+
+    const changeCourseCountBackward = (e) => {
+        e.stopPropagation();
+        setCourseSliderAnimate(
+          "course-slider__courses course-slider__slideout-right"
+        );
+        setTimeout(() => {
+          setCourseCount(courseCount - showCourses);
+          setCourseSliderAnimate(
+            "course-slider__courses course-slider__slidein-right"
+          );
+        }, 150);
+      };
+    
+      const changeCourseCountForward = (e) => {
+        e.stopPropagation();
+        setCourseSliderAnimate("course-slider__courses course-slider__slideout");
+        setTimeout(() => {
+          setCourseCount(courseCount + showCourses);
+          setCourseSliderAnimate("course-slider__courses course-slider__slidein");
+        }, 150);
+      };
+
+    console.log(activeList)
+
+
     return (
         <div className="course-slider">
             <div className="course-slider__title">Top Courses</div>
             <div className="course-slider__underline" />
             <div className="course-slider__container">
-                <div className="course-slider__course--backward" onClick={(e) => changeCourseCount(e, false) } >
-                    <FaArrowLeft />
-                </div>
+                {courseCount === 0 ? <div /> :
+                    <div className="course-slider__course--backward" onClick={(e) => changeCourseCountBackward(e, false)} >
+                        <FaArrowLeft />
+                    </div>
+                }
                 <div className={courseSliderAnimate}>
                     {activeList}
                 </div>
-                <div className="course-slider__course--forward" onClick={(e) => changeCourseCount(e, true)}>
+                {courseCount + showCourses >= courseCopy.length ?<div/>:
+                <div className="course-slider__course--forward" onClick={(e) => changeCourseCountForward(e, true)}>
                     <FaArrowRight />
                 </div>
+                }
             </div>
         </div>
     );
